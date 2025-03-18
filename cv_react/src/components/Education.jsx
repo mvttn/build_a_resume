ceebaed8a2b450ea3ea8b10ae68a8e541babfd89
mvttn <85101143+mvttn@users.noreply.github.com>
2025-03-18 21:55:18 +1100

    Change div tag to button tag

--- cv_react/src/components/Education.jsx
+++ cv_react/src/components/Education.jsx
@@ -7,7 +7,7 @@ export default function Education() {
 
   return (
     <div className="mx-10 my-5 block w-full max-w-sm min-w-[475px] justify-evenly rounded-lg border border-gray-200 bg-white px-6 shadow-sm">
-      <div
+      <button
         className={`flex h-[50px] w-full items-center justify-between transition-all duration-300 ease-in-out ${collapsed ? "rounded-xl" : "rounded-t-xl"}`}
         onClick={() => setCollapsed(!collapsed)}
       >
@@ -18,7 +18,7 @@ export default function Education() {
         <MdArrowBackIosNew
           className={`text-sm transition-transform duration-300 ${collapsed ? "rotate-90" : "-rotate-90"}`}
         />
-      </div>
+      </button>
       <div
         className={`flex w-full items-center justify-center rounded-b-xl transition-all duration-300 ease-in-out ${collapsed ? "h-0" : "h-[75px]"} m-0`}
       >
--- cv_react/src/components/PersonalDetails.jsx
+++ cv_react/src/components/PersonalDetails.jsx
@@ -8,7 +8,7 @@ export default function PersonalDetails() {
   return (
     <div className="mx-10 my-5 block w-full max-w-sm min-w-[475px] justify-evenly rounded-lg border border-gray-200 bg-white px-6 shadow-sm">
       {" "}
-      <div
+      <button
         className={`flex h-[50px] w-full items-center justify-between transition-all duration-300 ease-in-out ${collapsed ? "rounded-xl" : "rounded-t-xl"}`}
         onClick={() => setCollapsed(!collapsed)}
       >
@@ -19,7 +19,7 @@ export default function PersonalDetails() {
         <MdArrowBackIosNew
           className={`text-sm transition-transform duration-300 ${collapsed ? "rotate-90" : "-rotate-90"}`}
         />
-      </div>
+      </button>
       <div
         className={`flex w-full flex-col items-start justify-evenly rounded-b-xl transition-all duration-300 ease-in-out ${collapsed ? "h-0" : "h-[350px]"} m-0`}
       >
